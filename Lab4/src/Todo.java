69092acf24f5dafbaee08e0548f6be7d3601cf67
Skyler Sprecker <saspreck@gmail.com>
2019-02-11 19:40:04 -0600

    First Commit

---
+++ Lab4/src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-07
+OU ID_______________: 113384038
+OU EMAIL____________: saspreck@ou.edu
+
+Tasks:
+1. finish animal class method comparing objects
+2. write tests for the animal class methods
+
+Estimated Time (minutes):
+1. 10
+2. 20
+
+Actual Time (minutes):
+1. 10
+2. XXX
+*/}
