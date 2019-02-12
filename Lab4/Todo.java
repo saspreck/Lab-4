dbfe6edc9159ee6c2ce7563a5fd0a97724c39a95
Skyler Sprecker <saspreck@gmail.com>
2019-02-11 21:39:54 -0600

    Updated class

---
+++ Lab4/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-07
+OU ID_______________: 113384038
+OU EMAIL____________: saspreck@ou.edu
+
+Tasks:
+1. finish animal class method comparing objects
+2. write tests for the animal class methods
+3. finish equipment class
+4. write tests for the equipment class methods
+5. write tests for zoo class methods
+
+Estimated Time (minutes):
+1. 10
+2. 20
+3. 10
+4. 20
+5. 20
+
+Actual Time (minutes):
+1. 10
+2. 20
+3. 5
+4. 15
+5. 15
+*/}
