8389e437b6dde794e45fd904e3835ff48a198996
Skyler Sprecker <saspreck@gmail.com>
2019-02-19 14:57:12 -0600

    Finished parseAndExecute method

---
+++ Lab5/src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-19
+OU ID_______________: 113384038
+OU EMAIL____________: saspreck@ou.edu
+
+Tasks:
+1. complete parseAndExecute method
+2. XXX
+
+Estimated Time (minutes):
+1. 10
+2. XXx
+
+Actual Time (minutes):
+1. 10
+2. XXX
+*/}
